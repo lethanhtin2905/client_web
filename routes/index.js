6cb8326b6f85f0cdc88fdaf919cb30e379591365
Lê Thanh Tín <57804580+lethanhtin2905@users.noreply.github.com>
2020-12-03 11:04:58 +0700

    update and fix store

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var homeControllers = require('../controllers/home-controllers');
-
+var productControllers = require('../controllers/product-controllers');
 
 /* GET home page. */
 router.get('/', homeControllers.index);
+
+// Store
+router.get('/store', productControllers.displayProducts);
 
 module.exports = router;
