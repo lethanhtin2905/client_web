ee5e97d1e859ed7263213cc717ba48f5f89c01e6
Lê Thanh Tín <57804580+lethanhtin2905@users.noreply.github.com>
2020-12-30 23:39:42 +0700

    update registration function

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var accountControllers = require('../controllers/account-controllers');
+const { ensureAuthenticated } = require('../config/auth');
 
 // Register Page
 router.get('/register', accountControllers.registerPage);
 
 // Login Page
 router.get('/login', accountControllers.loginPage);
+
+// Activate Account Page
+router.get('/activate/:id', accountControllers.activatePage);
+
+// Register Handle
+router.post('/register', accountControllers.registerHandle);
 
 module.exports = router;
